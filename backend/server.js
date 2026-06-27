ebd88ee397e7015506715a1c95ef25a1adf31d29
a3ela <webwizabel@gmail.com>
2026-06-27 19:55:20 +0300

    chores: add inital react and express project

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const port = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
