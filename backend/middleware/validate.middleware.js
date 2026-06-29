8955dfcc60d1eb0f6b2328b649dd566304e7b54a
a3ela <webwizabel@gmail.com>
2026-06-29 10:18:28 +0300

    feats: validator for all incoming requests with validationResult

---
+++ backend/middleware/validate.middleware.js
@@ -0,0 +1,14 @@
+const { validationResult } = require("express-validator");
+
+const validateMiddleware = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      message: errors.array().map((e) => e.msg).join(", "),
+    });
+  }
+  next();
+};
+
+module.exports = validateMiddleware;
