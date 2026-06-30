2a76e396f5fcfa032e9815bbf6c2514ee1f683fe
a3ela <webwizabel@gmail.com>
2026-06-30 10:00:40 +0300

    feats: production ready

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/users", require("./routes/user.routes"));
 app.use("/api/products", require("./routes/product.routes"));
 app.use("/api/transactions", require("./routes/transaction.routes"));
@@ -24,6 +20,18 @@ app.use("/api/transactions", require("./routes/transaction.routes"));
 app.use((req, res) => {
   res.status(404).json({ success: false, message: "Route not found" });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 // Error handler (must be last)
 app.use(errorHandler);
